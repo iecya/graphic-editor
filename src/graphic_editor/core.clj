8caa1249e32d1717d32698817c9ec7a909c68de8
Federica Delisi <federica.delisi@style.com>
2016-12-22 20:35:02 +0000

    Add welcome and goodbye messages

--- src/graphic_editor/core.clj
+++ src/graphic_editor/core.clj
@@ -2,9 +2,18 @@
   (:gen-class)
   (:require [graphic-editor.api :as api]))
 
+
+(def welcome-msg (str "\n\n\n=================================================\n\n\n"
+                      (clojure.string/upper-case "***** Welcome to Graphic Editor *****")
+                      "\nTo quit the session, please type 'q', 'Q', 'quit' or 'QUIT'"
+                      "\n\n\n================================================="
+                      "\n\n\n"))
+
 (defn -main
   []
+  (println welcome-msg)
   (loop [s (read-line)]
-    (when-not (or (= s "q") (= s "Q") (= s "quit") (= s "QUIT"))
-      (api/input->function s)
-      (recur (read-line)))))
+    (if (or (= s "q") (= s "Q") (= s "quit") (= s "QUIT"))
+      "Bye Bye!"
+      (do (api/input->function s)
+          (recur (read-line))))))
