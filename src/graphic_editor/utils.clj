ba1609d2e3c03c734ec2cb53d437cad632b5ef3d
Federica Delisi <federica.delisi@style.com>
2016-12-23 12:36:17 +0000

    Pass img as argument and update functions body

--- src/graphic_editor/utils.clj
+++ src/graphic_editor/utils.clj
@@ -3,29 +3,34 @@
 
 (defn ?pixel
   "Checks if a pixel exists at the given coordinates"
-  [x y]
-  (some #(when (and (= x (:x %)) (= y (:y %))) %) (:items @image-data)))
+  [x' y' img]
+  (some (fn [{:keys [x y] :as px}]
+          (when (and (= x x') (= y y'))
+            px)) (:items img)))
+
 
 (defn ?pixel-color
   "If pixel exist, returns its color"
-  [x y]
-  (when-let [px (?pixel x y)]
+  [x y img]
+  (when-let [px (?pixel x y img)]
     (:color px)))
 
 (defn sort-items
-  []
-  (->> (:items @image-data)
+  [img]
+  (->> (:items img)
        (group-by :y)
        sort
        vals
        (mapv #(sort-by :x %))))
 
 (defn err-handler
-  [err-type & [arg]]
+  [err-type arg img]
   (println (case err-type
              :invalid-function (str "Sorry, function " arg " does not exist")
              :invalid-args (str "Sorry, function " arg " received a wrong number or type of arguments")
-             :invalid-pixel (str "Pixel at coordinates(" (:x arg) ", " (:y arg) ") is not in the image"))))
+             :invalid-pixel (str "Pixel at coordinates(" (:x arg) ", " (:y arg) ") is not in the image")))
+  img)
+
 
 (defn get-row
   [items]
