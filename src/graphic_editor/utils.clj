b8cd4fe2633e31fea363fafe300538f20c3972e8
Federica Delisi <federica.delisi@style.com>
2016-12-28 16:37:53 +0000

    Add description to all functions

--- src/graphic_editor/utils.clj
+++ src/graphic_editor/utils.clj
@@ -10,11 +10,13 @@
 
 
 (defn same-color?
+  "Returns true if the given pixels have the same color, false otherwise"
   [px0 px]
   (= (:color px0) (:color px)))
 
 
 (defn sort-items
+  "Returns the list of pixel sorted first by :y and then by :x"
   [img]
   (->> (:items img)
        (group-by :y)
@@ -24,6 +26,7 @@
 
 
 (defn v-segment
+  "Given the coords for a vertical segment, returns a new set of pixels after changing color to the pixels included in the segment area"
   [x' ys ye c]
   (fn [items]
     (into #{} (for [{:keys [x y] :as px} items]
@@ -35,6 +38,7 @@
 
 
 (defn h-segment
+  "Given the coords for a horizontal segment, returns a new set of pixels after changing color to the pixels included in the segment area"
   [xs xe y' c]
   (fn [items]
     (into #{} (for [{:keys [x y] :as px} items]
@@ -46,6 +50,7 @@
 
 
 (defn color-region
+  "Given the coords for region of neighbour pixels of the same color, returns a new set of pixels after changing color to the pixels included in the region"
   [region clr]
   (fn [items]
     (into #{} (for [px items]
@@ -57,8 +62,8 @@
 
 
 (defn get-siblings
-  [img]
   "Returns a function that, for the given pixel, returns a list of its valid siblings of the same color"
+  [img]
   (fn [{:keys [x y] :as px}]
     (let [siblings (list (?pixel x (inc y) img)
                          (?pixel x (dec y) img)
@@ -69,6 +74,7 @@
 
 
 (defn extend-list
+  "Returns a list formed by the given list and the siblings of each pixels that are not already included in the original list"
   [l img]
   (let [siblings         (mapcat (get-siblings img) l)
         missing-siblings (filter (fn [s] (when (every? #(not= s %) l) s)) siblings)]
@@ -76,6 +82,7 @@
 
 
 (defn fill
+  "Given a list of pixel and a color, extend the list with the siblings of the pixels. If the extended list is equal to the original list, color all the pixels in the list with the given color, otherwise extend the new list"
   [l c img]
   (let [extended-list (extend-list l img)]
     (if-not (= l extended-list)
@@ -84,6 +91,7 @@
 
 
 (defn err-handler
+  "Given an error type, print the related error message and return the image"
   [err-type arg img]
   (println (case err-type
              :invalid-function (str "Sorry, function " arg " does not exist")
@@ -93,6 +101,7 @@
 
 
 (defn get-row
+  "Given a list of sorted pixels, returns a string made of the color of each pixel"
   [items]
   (apply str (map :color items)))
 
