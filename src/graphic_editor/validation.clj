8d510d3d66fac6b3d922f5dc5a6bc0e5ece5fcf0
Federica Delisi <federica.delisi@style.com>
2016-12-28 18:28:47 +0000

    Simplify validations to a single function and remove unused arguments

--- src/graphic_editor/validation.clj
+++ src/graphic_editor/validation.clj
@@ -1,47 +1,23 @@
 (ns graphic-editor.validation)
 
 
-(defn validate-v-segment
-  [args img]
-  (let [coords (butlast args)
-        y-coords (rest coords)
-        x-coords (first coords)]
-      (and (every? integer? coords)
-           (re-find #"[A-Z]" (str (last args)))
-           (every? #(<= 1 % (:rows img)) y-coords)
-           (<= 1 x-coords (:cols img)))))
-
-
-(defn validate-h-segment
-  [args img]
-  (let [coords   (butlast args)
-        y-coords (last coords)
-        x-coords (butlast coords)]
+(defn args-validation
+  [args]
+  (let [coords   (butlast args)]
     (and (every? integer? coords)
-         (re-find #"[A-Z]" (str (last args)))
-         (every? #(<= 1 % (:cols img)) x-coords)
-         (<= 1 y-coords (:rows img)))))
-
-
-(defn single-px-coords
-  [args img]
-  (let [coords (butlast args)]
-    (and (every? integer? coords)
-         (re-find #"[A-Z]" (str (last args)))
-         (<= 1 (first coords) (:cols img))
-         (<= 1 (last coords) (:rows img)))))
+         (re-find #"[A-Z]" (str (last args))))))
 
 
 (defn validate-args
-  [f args img]
+  [f args]
   (case f
     "I" (every? #(and (integer? %)
                       (<= 1 % 250)) args)
     "C" (empty? args)
-    "L" (single-px-coords args img)
-    "V" (validate-v-segment args img)
-    "H" (validate-h-segment args img)
-    "F" (single-px-coords args img)
+    "L" (args-validation args)
+    "V" (args-validation args)
+    "H" (args-validation args)
+    "F" (args-validation args)
     "S" (empty? args)
     nil))
 
